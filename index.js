25e56127d20029563b0f4120dc85e320b1413686
Abu Al Nasir Mitul <nasirmitul28@gmail.com>
2022-11-25 00:36:45 +0600

    server all set and category, product data loaded

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000;
+require('dotenv').config()
+
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+//database connection
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.4w35qx6.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    console.log(authHeader);
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    const token = authHeader;
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next()
+    })
+}
+
+
+
+async function run() {
+    try {
+
+        const categoryCollection = client.db('drift').collection('categories');
+        const productsCollection = client.db('drift').collection('products');
+
+        //creating api for categories
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = categoryCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        })
+        
+        //creating api for single service
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { category_id: id };
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+
+        
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.log(err))
+
+
+
+app.get('/', (req, res) => {
+    res.send("Drift server is running")
+})
+
+
+//run server
+app.listen(port, () => {
+    console.log('server is running on port number', port);
+})
